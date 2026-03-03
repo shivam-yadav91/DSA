be797fbade9d05c7752d6172fec00d8919e56779
shivam-yadav91 <shivam.yadav_bca24@gla.ac.in>
2026-03-03 22:14:55 +0530

    Updated ReverseArrayEasy.java

--- ReverseArrayEasy.java
+++ ReverseArrayEasy.java
@@ -7,6 +7,7 @@ public class ReverseArrayEasy {
 
         for(int i = arr.length - 1; i >= 0; i--) {
             System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 }
