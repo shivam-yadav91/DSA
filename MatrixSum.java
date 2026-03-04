fcab6ac9955fd84fee0665a0d0af8dc64245cc3c
shivam-yadav91 <shivam.yadav_bca24@gla.ac.in>
2026-03-04 22:11:53 +0530

    Added MatrixSum file

---
+++ MatrixSum.java
@@ -0,0 +1,19 @@
+class MatrixSum {
+    public static void main(String[] args) {
+
+        int arr[][] = {
+            {1, 2},
+            {3, 4}
+        };
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
