65beff1280a111c7fc151c3d8856b61f279c9db0
shivam-yadav91 <shivam.yadav_bca24@gla.ac.in>
2026-03-09 14:32:06 +0530

    Added SecondLargest file

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,8 +1,17 @@
-import java.util.Arrays;
-public class SecondLargest {
-    public static void main(String[] args) {
-        int[] arr = {4,5,6,3,0};
-        Arrays.sort(arr);
-            System.out.println(arr[arr.length-2]);
+public class SecondLargest{
+    public static void main(String[]args){
+        int []a ={8,8,7,6,5};
+        int largest = Integer.MIN_VALUE;
+        int secondlargest = Integer.MIN_VALUE;
+        for(int i=0; i < a.length; i++){
+            if(a[i] > largest){
+                secondlargest = largest;
+                largest = a[i];
+            }
+            else if(a[i] > secondlargest && a[i] != largest){
+                secondlargest = a[i];
+            }
         }
+        System.out.println(secondlargest);
     }
+}
