72152feb6df50424bec92d5da334d45778cff8fd
shivam-yadav91 <shivam.yadav_bca24@gla.ac.in>
2026-03-12 16:54:26 +0530

    Added ArraySorted file

---
+++ ArraySorted.java
@@ -0,0 +1,17 @@
+public class ArraySorted {
+    public static boolean isSorted(int[]a){
+        for(int i =0; i<a.length -1; i++){
+            if(a[i] > a[i+1]){
+                return false;
+            }
+        }
+        
+        return true; 
+    }
+
+    public static void main(String[]args){
+        int[]a ={1,2,3,4,5};
+
+        System.out.println(isSorted(a));
+    }
+}
